5ef4bea32c338277868fc6af5a456243cda6cf6a
Omar Altamimi <ads.alsaheb@gmail.com>
2025-02-06 15:25:01 +0300

    thnx god

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,26 @@
+/// app-routing.module.ts
+
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeShellComponent } from './home-shell/home-shell.component';
+import { loadRemoteModule } from '@angular-architects/module-federation';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeShellComponent,
+    pathMatch: 'full'
+  },
+  {
+    path: 'remote',
+    loadChildren: () =>
+      loadRemoteModule({
+        type: 'module',
+        remoteEntry: 'http://localhost:4201/remoteEntry.js',
+        exposedModule: './RemoteMainModule',
+      }).then((m) => m.RemoteMainModule),
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/home-shell/home-shell.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home-shell',
+  standalone: false,
+  templateUrl: './home-shell.component.html',
+  styleUrl: './home-shell.component.scss'
+})
+export class HomeShellComponent {
+
+}
